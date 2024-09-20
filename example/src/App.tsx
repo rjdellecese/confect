ff0b8b7129dacc83579340513a2926e51c1eca91
RJ Dellecese <rj@rjdellecese.com>
2024-09-20 21:49:34 +0000

    Add `useQuery` and `useMutation` hooks

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,67 @@
+/// <reference types="vite/client" />
+
+import { useQuery, useMutation } from "@rjdellecese/confect/react";
+import { api } from "../convex/_generated/api";
+import { confectTableSchemas } from "../convex/schema";
+import { Schema } from "@effect/schema";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Array, Effect, Option } from "effect";
+import { useState } from "react";
+import { Id } from "@rjdellecese/confect/server";
+
 const App = () => {
+	const convexClient = new ConvexReactClient(
+		// biome-ignore lint/complexity/useLiteralKeys: TS error otherwise
+		import.meta.env["VITE_CONVEX_URL"],
+	);
+
+	return (
+		<ConvexProvider client={convexClient}>
+			<Page />
+		</ConvexProvider>
+	);
+};
+
+const Page = () => {
+	const [note, setNote] = useState("");
+	const insertNote = useMutation({
+		mutation: api.functions.insertNote,
+		args: Schema.Struct({ text: Schema.String }),
+		returns: Id.Id<"notes">(),
+	});
+
 	return (
 		<div>
-			<h1>Welcome to Confect Example</h1>
-			<p>This is a simple React page using Vite.</p>
+			<h1>Confect Example</h1>
+
+			<textarea value={note} onChange={(e) => setNote(e.target.value)} />
+			<button
+				type="button"
+				onClick={() => insertNote({ text: note }).pipe(Effect.runPromise)}
+			>
+				Insert
+			</button>
+
+			<NoteList />
 		</div>
+	);
+};
+
+const NoteList = () => {
+	const notes = useQuery({
+		query: api.functions.listNotes,
+		args: Schema.Struct({}),
+		returns: Schema.Array(confectTableSchemas.notes.withSystemFields),
+	})({});
+
+	return (
+		<ul>
+			{Option.match(notes, {
+				onNone: () => <li>Loading…</li>,
+				onSome: (notes) =>
+					Array.map(notes, (note) => <li key={note._id}>{note.text}</li>),
+			})}
+		</ul>
 	);
 };
 
