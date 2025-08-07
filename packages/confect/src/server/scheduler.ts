7c1612f87c315a4d59c5db29dee428fb269033d1
RJ Dellecese <rj@rjdellecese.com>
2025-08-07 22:58:51 +0000

    Review more TODOs

--- packages/confect/src/server/scheduler.ts
+++ packages/confect/src/server/scheduler.ts
@@ -13,9 +13,8 @@ const make = (scheduler: Scheduler) => ({
   ) => {
     const delayMs = Duration.toMillis(delay);
 
-    // TODO: Which errors might occur?
     return Effect.promise(() =>
-      scheduler.runAfter(delayMs, functionReference, ...args),
+      scheduler.runAfter(delayMs, functionReference, ...args)
     );
   },
   runAt: <FuncRef extends SchedulableFunctionReference>(
@@ -25,15 +24,14 @@ const make = (scheduler: Scheduler) => ({
   ) => {
     const timestamp = DateTime.toEpochMillis(dateTime);
 
-    // TODO: Which errors might occur?
     return Effect.promise(() =>
-      scheduler.runAt(timestamp, functionReference, ...args),
+      scheduler.runAt(timestamp, functionReference, ...args)
     );
   },
 });
 
 export class ConfectScheduler extends Effect.Tag(
-  "@rjdellecese/confect/ConfectScheduler",
+  "@rjdellecese/confect/ConfectScheduler"
 )<ConfectScheduler, ReturnType<typeof make>>() {
   static readonly layer = (scheduler: Scheduler) =>
     Layer.succeed(this, make(scheduler));
