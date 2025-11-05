023b426d02b369812fd1c46a59470ed9d783a396
RJ Dellecese <rj@rjdellecese.com>
2025-11-05 06:43:49 +0000

    Fix test

--- packages/confect/src/api/ConfectApiBuilder.test.ts
+++ packages/confect/src/api/ConfectApiBuilder.test.ts
@@ -1,25 +1,16 @@
 import { describe, expectTypeOf, test } from "vitest";
-import { GenericConfectSchema } from "../server/schema";
 import * as ConfectApiBuilder from "./ConfectApiBuilder";
-import * as ConfectApiGroup from "./ConfectApiGroup";
 
 describe("ConfectApiBuilder", () => {
   describe("ConfectApiGroupService", () => {
     test("is excludable", () => {
-      type ConfectSchema = GenericConfectSchema;
-
-      type GroupA = ConfectApiGroup.ConfectApiGroup<ConfectSchema, "groupA">;
-      type GroupB = ConfectApiGroup.ConfectApiGroup<ConfectSchema, "groupB">;
-
       type GroupAService = ConfectApiBuilder.ConfectApiGroupService<
-        ConfectSchema,
         "api",
-        GroupA
+        "groupA"
       >;
       type GroupBService = ConfectApiBuilder.ConfectApiGroupService<
-        ConfectSchema,
         "api",
-        GroupB
+        "groupB"
       >;
 
       type ExcludedGroupAService = Exclude<
