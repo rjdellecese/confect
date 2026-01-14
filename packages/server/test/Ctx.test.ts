52415333ac795a9cd1417c7155f734d3dc3208bf
RJ Dellecese <rj@rjdellecese.com>
2026-01-14 06:37:00 +0000

    Use `layer` from `@effect/vitest` for `ConfectTest`ing

--- packages/server/test/Ctx.test.ts
+++ packages/server/test/Ctx.test.ts
@@ -1,15 +1,14 @@
-import { describe, expect } from "@effect/vitest";
+import { expect, layer } from "@effect/vitest";
 import { assertEquals } from "@effect/vitest/utils";
 import { Effect, Option } from "effect";
 import { api } from "./confect/_generated/refs";
 import { DatabaseWriter } from "./confect/_generated/services";
-import { TestConfect } from "./TestConfect";
-import { effect } from "./testUtils";
+import * as TestConfect from "./TestConfect";
 
-describe("QueryCtx", () => {
-  effect("should get a note", () =>
+layer(TestConfect.layer)("QueryCtx", (it) => {
+  it.effect("should get a note", () =>
     Effect.gen(function* () {
-      const c = yield* TestConfect;
+      const c = yield* TestConfect.TestConfect;
 
       const text = "Hello, world!";
 
@@ -32,10 +31,10 @@ describe("QueryCtx", () => {
   );
 });
 
-describe("MutationCtx", () => {
-  effect("should insert a note", () =>
+layer(TestConfect.layer)("MutationCtx", (it) => {
+  it.effect("should insert a note", () =>
     Effect.gen(function* () {
-      const c = yield* TestConfect;
+      const c = yield* TestConfect.TestConfect;
 
       const text = "Hello, world!";
 
@@ -45,17 +44,17 @@ describe("MutationCtx", () => {
 
       const note = yield* c
         .query(api.groups.notes.getFirst, {})
-        .pipe(Effect.map(Option.map(({ text }) => text)));
+        .pipe(Effect.map(Option.map((note_) => note_.text)));
 
       assertEquals(note, Option.some(text));
     }),
   );
 });
 
-describe("ActionCtx", () => {
-  effect("should insert a note", () =>
+layer(TestConfect.layer)("ActionCtx", (it) => {
+  it.effect("should insert a note", () =>
     Effect.gen(function* () {
-      const c = yield* TestConfect;
+      const c = yield* TestConfect.TestConfect;
 
       const randomNumber = yield* c.action(api.groups.random.getNumber, {});
 
