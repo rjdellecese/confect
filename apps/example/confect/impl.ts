49a791f3652f2376c8ed139c0e17debf3b76aa2e
RJ Dellecese <rj@rjdellecese.com>
2026-02-10 03:04:43 +0000

    Tidy up example impl

--- apps/example/confect/impl.ts
+++ apps/example/confect/impl.ts
@@ -2,12 +2,8 @@ import { Impl } from "@confect/server";
 import { Layer } from "effect";
 import api from "./_generated/api";
 import { notesAndRandom } from "./impl/notesAndRandom";
-import { notes } from "./impl/notesAndRandom/notes";
-import { random } from "./impl/notesAndRandom/random";
 
 export default Impl.make(api).pipe(
-  Layer.provide(
-    notesAndRandom.pipe(Layer.provide(notes), Layer.provide(random)),
-  ),
+  Layer.provide(notesAndRandom),
   Impl.finalize,
 );
--- packages/server/test/convex/groups/notes.ts
+++ packages/server/test/convex/groups/notes.ts
@@ -1,7 +1,7 @@
 import registeredFunctions from "../../confect/_generated/registeredFunctions";
 
-export const internalGetFirst = registeredFunctions.groups.notes.internalGetFirst;
-export const getFirst = registeredFunctions.groups.notes.getFirst;
 export const delete_ = registeredFunctions.groups.notes.delete_;
-export const list = registeredFunctions.groups.notes.list;
+export const getFirst = registeredFunctions.groups.notes.getFirst;
 export const insert = registeredFunctions.groups.notes.insert;
+export const internalGetFirst = registeredFunctions.groups.notes.internalGetFirst;
+export const list = registeredFunctions.groups.notes.list;
