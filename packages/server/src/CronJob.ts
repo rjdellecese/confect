2dc27371cbf43fa0f79fe7ec58420e6569c0d4fd
RJ Dellecese <rj@rjdellecese.com>
2026-03-29 02:30:04 +0000

    Add Effect- & Confect-native support for cron jobs

---
+++ packages/server/src/CronJob.ts
@@ -0,0 +1,38 @@
+import type { Ref } from "@confect/core";
+import type { Cron, Duration } from "effect";
+import { Predicate } from "effect";
+
+export const TypeId = "@confect/server/CronJob";
+export type TypeId = typeof TypeId;
+
+export interface CronJob {
+  readonly [TypeId]: TypeId;
+
+  readonly identifier: string;
+  readonly schedule: Cron.Cron | Duration.Duration;
+  readonly ref: Ref.AnyMutation | Ref.AnyAction;
+}
+
+export const isCronJob = (u: unknown): u is CronJob =>
+  Predicate.hasProperty(u, TypeId);
+
+const Proto = {
+  [TypeId]: TypeId,
+};
+
+const makeProto = (
+  identifier: string,
+  schedule: Cron.Cron | Duration.Duration,
+  ref: Ref.AnyMutation | Ref.AnyAction,
+): CronJob =>
+  Object.assign(Object.create(Proto), {
+    identifier,
+    schedule,
+    ref,
+  });
+
+export const make = (
+  identifier: string,
+  schedule: Cron.Cron | Duration.Duration,
+  ref: Ref.AnyMutation | Ref.AnyAction,
+): CronJob => makeProto(identifier, schedule, ref);
